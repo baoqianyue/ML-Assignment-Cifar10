6c78a6ffb5c31fc6a7709da7af4abac192024759
MIKIMIKA <zhbh.tjyrz@gmail.com>
2019-06-20 22:21:42 +0800

    compute distances one loop

--- knn-classification/classwork/DSVC/classifiers/k_nearest_neighbor.py
+++ knn-classification/classwork/DSVC/classifiers/k_nearest_neighbor.py
@@ -94,7 +94,7 @@ class KNearestNeighbor(object):
             # Compute the l2 distance between the ith test point and all training #
             # points, and store the result in dists[i, :].                        #
             #######################################################################
-            pass
+            dists[i] = np.sqrt(np.sum(np.square(X[i]-self.X_train),axis=1))
             #######################################################################
             #                         END OF YOUR CODE                            #
             #######################################################################
