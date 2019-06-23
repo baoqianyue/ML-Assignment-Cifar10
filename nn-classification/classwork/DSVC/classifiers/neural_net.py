34bdebe3729645df1a07c0bc9d946505d3172daa
jjl1203 <jjl743726293@qq.com>
2019-06-23 23:18:43 +0800

    ti jiao chao can2

--- nn-classification/classwork/DSVC/classifiers/neural_net.py
+++ nn-classification/classwork/DSVC/classifiers/neural_net.py
@@ -215,8 +215,8 @@ class TwoLayerNet(object):
             #                             END OF YOUR CODE                          #
             #########################################################################
 
-            if verbose and it % 100 == 0:
-               print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+            if verbose and it % 500 == 0:
+                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
             # Every epoch, check train and val accuracy and decay learning rate.
             if it % iterations_per_epoch == 0:
